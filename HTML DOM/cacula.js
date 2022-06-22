3f0b918d149dea192a736efaeb17ab51a076cd48
Nguyen-Tang-Nghia-77 <101049903+Nguyen-Tang-Nghia-77@users.noreply.github.com>
2022-06-22 09:00:16 +0700

    create

---
+++ HTML DOM/cacula.js
@@ -0,0 +1,14 @@
+// function add() {
+//     let num1 = +document.getElementById('num1').value;
+//     let num2 = +document.getElementById('num2').value;
+//     let sum = num1 + num2;
+//     let result = "Kết quả là" + num1 + num2 +"=" + sum;
+//     document.getElementById('result').innerHTML=result;
+// }
+// function sub() {
+//     let num1 = document.getElementById('num1').value;
+//     let num2 = document.getElementById('num2').value;
+//     let sum = num1 + num2;
+//     let result = "Kết quả là" + num1 - num2 +"=" + sub;
+//     document.getElementById('result').innerHTML=result;
+// }
